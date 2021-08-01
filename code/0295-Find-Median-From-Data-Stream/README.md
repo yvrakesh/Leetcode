fabb7a82cea019538eecce734c391f4ab5ebbfb0
Yacha Venkata Rakesh <venkatarakesh1234y@gmail.com>
2021-08-01 22:56:00 +0530

    295. Find Median from data stream Leetcode Solutions Github

--- code/0295-Find-Median-From-Data-Stream/README.md
+++ code/0295-Find-Median-From-Data-Stream/README.md
@@ -27,8 +27,23 @@ Balanced Binary Tree is best suitable here. (Insertion here is O(n))
 *Problem here* is if we use a heap we can only get either minimum or maximum elements. But we need middle element.
 
 Approach: 
+    
+    Time Complexity : O(logn) for addNum and O(1) for findMedian()
+
+    Space Complexity : O(n) for priority queues.
 
 1. Divide the array into two parts. Implement max heap on left half and min heap on right half.
 2. If total number of elements are even then fetch the max heap from 1st half and min heap from second half.
 3. Else fetch max heap from the first half.
 4. Insertion: Compare insertion element with max heap of the 1st half and if it is less than 
+
+Brief overview:
+
+1. We use 2 heaps to track the median.
+2. Left half is stored in max heap to get the maximum in left half.
+3. Right half is stored in min heap to get the minimum in right half.
+4. We make sure that either 
+5. Min heap size equal to Max heap size in case of even length.
+6. In this case add element to right half, extract min of it and add it to left half.
+7. Max heap size is 1 more than min heap size in case of odd length.
+8. In this case add element to left half, extract max of it and add it to right half.(To make both equal).
--- code/0295-Find-Median-From-Data-Stream/sol.cpp
+++ code/0295-Find-Median-From-Data-Stream/sol.cpp
@@ -7,57 +7,36 @@ class MedianFinder {
     // Min heap for 2nd half
     priority_queue<int, vector<int>, greater<int>> min_heap;
 public:
-    /** initialize your data structure here. */
+    // Constructor
     MedianFinder() {
         ios_base::sync_with_stdio(false);    
         cin.tie(NULL);
-        this->median = 0;
     }
     // Inserting a new element
     void addNum(int num) {
-        // Check if both min heap and max heap sizes are equal
+        // Increase left half size in case of equal count
         if(max_heap.size() == min_heap.size()){
-            // So now as both are of equal size we can add to either of them.
-            // So let's do based on the median.
-            // If the number is less than the previous median, then insert it to the left half.
-            if((double)num < median){
-                max_heap.push(num);
-                median = max_heap.top();
-            }
-            // Else insert it to the right half
-            else{
-                min_heap.push(num);
-                median = min_heap.top();
-            }
+            min_heap.push(num);
+            max_heap.push(min_heap.top());
+            min_heap.pop();
         }
         // If left half size is greater than right half size
         else if(max_heap.size() > min_heap.size()){
-            // If number is less than median insert 1st half top to the 2nd half.
-            // Pop 1st half element and insert the number.
-            if((double)num < median){
-                min_heap.push(max_heap.top());
-                max_heap.pop();
-                max_heap.push(num);
-            }
-            else{
-                min_heap.push(num);
-            }
-            median = (max_heap.top() + min_heap.top())/2.0;
-        }
-        else{
-            if((double)num > median){
-                max_heap.push(min_heap.top());
-                min_heap.pop();
-                min_heap.push(num);
-            }
-            else{
-                max_heap.push(num);
-            }
-            median = (max_heap.top() + min_heap.top())/2.0;
+            max_heap.push(num);
+            min_heap.push(max_heap.top());
+            max_heap.pop();
         }
     }
     
-    double findMedian() {
-        return median;
+    double findMedian(){
+        // If no element is present
+        if(max_heap.empty())
+            return 0;
+        // If left half count equal to right half count
+        // i.e. if even size so return average of both the middle elements
+        if(max_heap.size() == min_heap.size())
+            return (max_heap.top() + min_heap.top())/2.0;
+        // return the middle element
+        return max_heap.top();
     }
 };
